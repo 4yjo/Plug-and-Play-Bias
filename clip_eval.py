7dfb5ca9f3116d7216f266e1baec5a92bd844130
4yjo <27517870+4yjo@users.noreply.github.com>
2024-01-29 15:47:39 +0000

    get final w for generation from wandb run

--- clip_eval.py
+++ clip_eval.py
@@ -1,21 +1,47 @@
 from PIL import Image
-#import requests
+
 import wandb
-
-
 from transformers import CLIPProcessor, CLIPModel
+from utils.stylegan import create_image 
 
 model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
 processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 
-#url = "http://images.cocodataset.org/val2017/000000039769.jpg"
-#image = Image.open(requests.get(url, stream=True).raw)
+#if image is already in folder
+#image = Image.open("/workspace/media_images_final_images_0_d263f1d46c6d5902de56.png")
+
+#reconstruct images from wandb run id 
+
+#get final weights from speciefied run
+api = wandb.Api(timeout=60)
+run = api.run("model_inversion_attacks/ga0mt8yu")
+
+
+for file in run.files():
+    #if file.name.startswith("media/images/final_"):
+    if file.name.startswith("results/optimized_w_selected"):    
+        print(file)
+        file.download("results", exist_ok=True)
+        
 
 
 
-inputs = processor(text=["a photo of a cat", "a photo of a dog"], images=image, return_tensors="pt", padding=True)
+ #img_results = create_image(final_w,
+  #                          generator,
+                            #crop_size=config.attack_center_crop,
+                            #resize=config.attack_resize,
+   #                         device=device).cpu()
+
+'''
+
+#prompt
+prompts = ["a photo of a person without beard", "a photo of a person with beard"]
+inputs = processor(text=prompts, images=image, return_tensors="pt", padding=True)
 
 outputs = model(**inputs)
 logits_per_image = outputs.logits_per_image  # this is the image-text similarity score
 probs = logits_per_image.softmax(dim=1)  # we can take the softmax to get the label probabilities
+print(logits_per_image[0])
+print(probs)
 
+'''
