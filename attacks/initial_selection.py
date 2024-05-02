0c21c45e933ed1168680a4e4e11ff355c0a9b5f3
4yjo <27517870+4yjo@users.noreply.github.com>
2024-05-02 09:33:13 +0000

    test array equals image

--- attacks/initial_selection.py
+++ attacks/initial_selection.py
@@ -104,25 +104,26 @@ def find_initial_w(generator,
                     target_conf = target_model(im).softmax(dim=1) / len(imgs)
             confidences.append(target_conf)
 
-            #bias_attr = [] # TODO what happens here? Initialize new list for each w?
+           #bias_attr = [] # TODO what happens here? Initialize new list for each w?
             for im in imgs:
                 for i in range(len(im)):
                     im[i] = (im[i] * 0.5 + 128 / 224).clamp(0, 1) #maps from [-1,1] to [0,1]
 
                     #TODO make this more elegant?
                     # match dimensions for CLIP processor
-                    #perm= im[i].permute(1, 2, 0) 
-                    #perm_rescale = perm.mul(255).add_(0.5).clamp_(0, 255).type(torch.uint8)
+                    perm= im[i].permute(1, 2, 0) 
+                    perm_rescale = perm.mul(255).add_(0.5).clamp_(0, 255).type(torch.uint8)
 
                     # reference 
-                    #torchvision.utils.save_image(im[i], 'media/test/test.png')
-                    #test1 = Image.open('media/test/test.png')
-                    #test1_arr = np.asarray(test1) 
-            
-                    torchvision.utils.save_image(im[i], 'media/test/temp_img.png') #save image for testing
-                    inputs = clip_processor(text=prompt, images=Image.open('media/test/temp_img.png'), return_tensors="pt", padding=True)
+                    torchvision.utils.save_image(im[i], 'media/test/test.png')
+                    test1 = Image.open('media/test/test.png')
+                    test1_arr = np.asarray(test1) 
 
-                    #inputs = clip_processor(text=prompt, images=perm_rescale, return_tensors="pt", padding=True)
+            
+                    #torchvision.utils.save_image(im[i], 'media/test/temp_img.png') #save image for testing
+                    #inputs = clip_processor(text=prompt, images=Image.open('media/test/test.png'), return_tensors="pt", padding=True)
+
+                    inputs = clip_processor(text=prompt, images=perm_rescale, return_tensors="pt", padding=True)
                     
                     outputs = clip_model(**inputs)
                     logits_per_image = outputs.logits_per_image  # this is the image-text similarity score
@@ -130,9 +131,15 @@ def find_initial_w(generator,
                     bias_attr = torch.argmax(probs).item()
                     bias_attributes.append(bias_attr)
             
-        print('bias attributes', bias_attributes)
+        #print('bias attributes', bias_attributes)
 
-        
+
+        results_test_image = [0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1]
+        results_perm_magic =  [0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1]
+
+        print(np.array_equal(results_perm_magic,results_test_image))
+        print(np.array_equal(perm_rescale.detach().cpu().numpy(), test1_arr))
+
         print('conf shape',len(confidences))
         confidences = torch.cat(confidences, dim=0)
         print('conf shape cat',len(confidences))
