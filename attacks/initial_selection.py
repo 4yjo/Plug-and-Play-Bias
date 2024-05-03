fc553e1750ab66b1253a241b3e6273cdb82a6e34
4yjo <27517870+4yjo@users.noreply.github.com>
2024-05-03 13:16:12 +0000

    wip bias in selection

--- attacks/initial_selection.py
+++ attacks/initial_selection.py
@@ -62,6 +62,9 @@ def find_initial_w(generator,
     outdir = "media/test" 
     os.makedirs(outdir, exist_ok=True)
 
+    outdir2 = "media/test2" 
+    os.makedirs(outdir2, exist_ok=True)
+
     with torch.no_grad():
         confidences = []
         bias_attributes = []
@@ -89,22 +92,37 @@ def find_initial_w(generator,
             if resize is not None:
                 imgs = [F.resize(imgs, resize, antialias=True)]
             if horizontal_flip:
-                imgs.append(F.hflip(imgs[0]))
+                imgs.append(F.hflip(imgs[0])) # adds copy of horizontallly flipped images -> len(imgs) changes from 1 to 2 
             if five_crop:
                 cropped_images = []
                 for im in imgs:
                     cropped_images += list(F.five_crop(im))
                 imgs = cropped_images
 
+            #print('imgs len',len(imgs))
+            #print('imgs 0', len(imgs[0]))
+            #print('imgs 1', len(imgs[1]))
+            #print('imgs 0 shape', imgs[0].shape)
+      
+
+            #print(torch.equal(imgs[0],imgs[1])) -> not equal because flipped
+
             target_conf = None
+            
             for im in imgs:
+                #print('im ', len(im))
+                #torchvision.utils.save_image(im, f'media/test/{np.random.randint(100)}.png') #save image for testing
                 if target_conf is not None:
                     target_conf += target_model(im).softmax(dim=1) / len(imgs)
                 else:
                     target_conf = target_model(im).softmax(dim=1) / len(imgs)
             confidences.append(target_conf)
+            #print('len conf', len(confidences))
+            #print('shpae conf [0]', confidences[0].shape)
+            #print(confidences[0])
+           
 
-           #bias_attr = [] # TODO what happens here? Initialize new list for each w?
+            bias_attr = []
             for im in imgs:
                 for i in range(len(im)):
                     im[i] = (im[i] * 0.5 + 128 / 224).clamp(0, 1) #maps from [-1,1] to [0,1]
@@ -115,9 +133,10 @@ def find_initial_w(generator,
                     perm_rescale = perm.mul(255).add_(0.5).clamp_(0, 255).type(torch.uint8)
 
                     # reference 
-                    torchvision.utils.save_image(im[i], 'media/test/test.png')
-                    test1 = Image.open('media/test/test.png')
-                    test1_arr = np.asarray(test1) 
+                    #torchvision.utils.save_image(im[i], 'media/test/test.png')
+                    #test1 = Image.open('media/test/test.png')
+                    #test1_arr = np.asarray(test1) 
+                    #print(np.array_equal(perm_rescale.detach().cpu().numpy(), test1_arr))
 
             
                     #torchvision.utils.save_image(im[i], 'media/test/temp_img.png') #save image for testing
@@ -127,29 +146,79 @@ def find_initial_w(generator,
                     
                     outputs = clip_model(**inputs)
                     logits_per_image = outputs.logits_per_image  # this is the image-text similarity score
-                    probs = logits_per_image.softmax(dim=1)  # 0 -> with glasses, 1 -> with no glasses
-                    bias_attr = torch.argmax(probs).item()
-                    bias_attributes.append(bias_attr)
+                    probs = torch.round(logits_per_image.softmax(dim=1)) 
+                
+
+                    bias_attr.append(probs)
             
-        #print('bias attributes', bias_attributes)
+                
+            print('bias attr 1', len(bias_attr))
 
-
-        results_test_image = [0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1]
-        results_perm_magic =  [0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1]
-
-        print(np.array_equal(results_perm_magic,results_test_image))
-        print(np.array_equal(perm_rescale.detach().cpu().numpy(), test1_arr))
-
+            bias_attr = torch.cat(bias_attr, dim=0)
+            print('bias attr 2', bias_attr.shape)
+            
+            bias_attributes.append(bias_attr)
+            #bias_attr = torch.argmax(probs).item() # 0 -> with glasses, 1 -> with no glasses
+        
+            
+        
+       
         print('conf shape',len(confidences))
         confidences = torch.cat(confidences, dim=0)
         print('conf shape cat',len(confidences))
-        #bias_attributes = torch.cat(bias_attributes, dim=0)
+         #print('conf cat shape 0', confidences[0].shape)
+            
+        print('bias attr3 ', len(bias_attributes))
+        bias_attributes = torch.cat(bias_attributes, dim=0)
+        print('bias attr 3', len(bias_attributes))
+
+
+        print("BIAAS")
+        print(bias_attributes[:10])
+        
+
 
         for target in targets:
+            print('TARGET X')
             # find candidate with highest confidence for each target
             sorted_conf, sorted_idx = confidences[:,
                                                   target].sort(descending=True)
             
+            print('len sorted conf', len(sorted_conf)) # -> 100
+
+            splitted_bias = bias_attributes[:,target]
+            print('len splitted', len(splitted_bias))
+            print(splitted_bias.shape) #-> torch.Size([100])
+
+            c_bias_attr = []
+            c_no_bias_attr = []
+
+            
+            for i in range(len(sorted_idx)):
+                if (splitted_bias[i].cpu().item() == 1):
+                    c_bias_attr.append(splitted_bias[sorted_idx])
+                else:
+                    c_no_bias_attr.append(splitted_bias[sorted_idx])
+            
+            print(c_bias_attr)
+            print(c_no_bias_attr)
+
+            # final idx
+            ratio = 0.5 # TODO Take ratio from attack file
+            c_bias_attr_cut = c_bias_attr[:(len(c_bias_attr)*ratio)]
+            c_no_bias_attr_cut = c_no_bias_attr[:(len(c_no_bias_attr)*(1-ratio))]
+
+            final_idx = c_bias_attr_cut.append(c_no_bias_attr_cut)
+            print(len(final_idx))
+
+
+
+            
+            
+
+           
+
+
             #while bias_counter < 0.5: # TODO import actual ratio
             
             # TODO implement CLIP check for bias attribute here as second point for choice
@@ -170,11 +239,13 @@ def find_initial_w(generator,
             #    sorted_idx.pop()
             #    sorted_conf.pop()
 
+            # TODO put back in if not kept above
             final_candidates.append(candidates[sorted_idx[0]].unsqueeze(0)) #get image with hightes confidence
             final_confidences.append(sorted_conf[0].cpu().item())
             # Avoid identical candidates for the same target
             confidences[sorted_idx[0], target] = -1.0
 
+    print('cand', candidates.shape)
 
     final_candidates = torch.cat(final_candidates, dim=0).to(device)
     final_confidences = [np.round(c, 2) for c in final_confidences]
